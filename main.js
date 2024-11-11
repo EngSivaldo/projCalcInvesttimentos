472f161f4cd33c7626532c60a0c02d332669d501
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-11-11 11:06:02 -0300

    O Evento de Blur, seu alvo e a inteligência de detecção do erro

--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 import { generateReturnsArray } from "./src/investimentGoals";
 
 const form = document.getElementById('investment-form');
-// const calculateButton = document.getElementById('calculate-results');
-
 
 if (form) {
   form.addEventListener('submit', renderProgression);
@@ -14,43 +12,54 @@ if (form) {
 function renderProgression(evt) {
   evt.preventDefault();
   // Verifique e obtenha todos os elementos necessários do DOM
-  // const startingAmountElement = Number(form['starting-amount'].value);//outra forma acessar value atraves do name
-  const startingAmountElement = document.getElementById('starting-amount');
-  const additionalContributionElement = document.getElementById('additional-contribution');
-  const timeAmountElement = document.getElementById('time-amount');
-  const timeAmountPeriodElement = document.getElementById('time-amount-period');
-  const returnRateElement = document.getElementById('return-rate');
-  const returnRatePeriodElement = document.getElementById('return-rate-period');
-  const taxRateElement = document.getElementById('tax-rate');
-  
-  // Verifique se todos os elementos foram encontrados
-  if (startingAmountElement && additionalContributionElement && timeAmountElement && 
-      timeAmountPeriodElement && returnRateElement && returnRatePeriodElement && taxRateElement) {
+  const startingAmount = Number(document.getElementById('starting-amount').value.replace(",", "."));
+  const additionalContribution = Number(document.getElementById('additional-contribution').value.replace(",", "."));
+  const timeAmount = Number(document.getElementById('time-amount').value);
+  const timeAmountPeriod = document.getElementById('time-amount-period').value;
+  const returnRate = Number(document.getElementById('return-rate').value.replace(",", "."));
+  const returnRatePeriod = document.getElementById('return-rate-period').value;
+  const taxRate = Number(document.getElementById('tax-rate').value.replace(",", "."));
 
-    // Obtenha os valores dos elementos
-    const startingAmount = Number(startingAmountElement.value);
-    const additionalContribution = Number(additionalContributionElement.value);
-    const timeAmount = Number(timeAmountElement.value);
-    const timeAmountPeriod = timeAmountPeriodElement.value;
-    const returnRate = Number(returnRateElement.value);
-    const returnRatePeriod = returnRatePeriodElement.value;
-    const taxRate = Number(taxRateElement.value);
+  // Gere o array de retornos
+  const returnsArray = generateReturnsArray(
+    startingAmount,
+    timeAmount,
+    timeAmountPeriod,
+    additionalContribution,
+    returnRate,
+    returnRatePeriod
+  );
 
-    // Gere o array de retornos
-    const returnsArray = generateReturnsArray(
-      startingAmount,
-      timeAmount,
-      timeAmountPeriod,
-      additionalContribution,
-      returnRate,
-      returnRatePeriod
-    );
+  console.log(returnsArray);
+}
 
-    console.log(returnsArray);
-  } else {
-    console.error('Um ou mais elementos necessários não foram encontrados no DOM');
+// funcao de exibir o error, quando digitar algo errado no input com class === name
+function validateInput(evt) {
+  if (evt.target.value === '') {
+    return;
+  }
+
+  const parentElement = evt.target.parentElement; // elemento pai
+  const grandParentElement = evt.target.parentElement.parentElement; // elemento avô
+  const inputValue = evt.target.value.replace(",", ".");
+
+  if (isNaN(inputValue) || Number(inputValue) <= 0) {
+    // objetivo: <p class="text-red-500">Insira um valor numerico e maior que zero</p>
+    const errorTextElement = document.createElement("p"); // <p></p>
+    errorTextElement.classList.add('text-red-500'); // <p class='text-red-500'></p>
+    errorTextElement.innerText = "Insira um valor numerico e maior que zero"; // <p class="text-red-500">Insira um valor numerico e maior que zero</p>
+
+    parentElement.classList.add('error'); // recebe borda vermelha (error)
+    grandParentElement.appendChild(errorTextElement);
   }
 }
 
+// Adicionar event listeners para blur
+const inputs = document.querySelectorAll('input[name]');
+inputs.forEach(input => {
+  input.addEventListener('blur', validateInput);
+});
+
+
+
 form.addEventListener('submit', renderProgression);
-// calculateButton.addEventListener('submit', renderProgression);
