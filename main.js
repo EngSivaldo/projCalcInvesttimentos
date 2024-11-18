4e0ce4f2154f12a4366eaa380595795c5d16807a
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-11-18 11:48:59 -0300

    Criando o carrossel - Inteligência dos botões

--- main.js
+++ main.js
@@ -200,6 +200,22 @@ for (const formElement of form) {
   }
 }
 
+//logica do carrossel
+const mainEl = document.querySelector('main');
+const carouselEl = document.getElementById('carousel');
+const nextButton = document.getElementById('slide-arrow-next')
+const previousButton = document.getElementById('slide-arrow-previous')
+
+nextButton.addEventListener('click', () => {
+  carouselEl.scrollLeft += mainEl.clientWidth;
+});
+
+previousButton.addEventListener('click', () => {
+  carouselEl.scrollLeft -= mainEl.clientWidth;
+});
+
+
+
 // Adiciona novamente o evento de submissão ao formulário
 form.addEventListener('submit', renderProgression);
 // calculateButton.addEventListener('click', renderProgression);
